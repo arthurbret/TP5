b94253fe69ffad5f73ce0be598f8afa4cfc0f14d
abret <arthur.bret@edu.ece.fr>
2022-11-17 09:30:01 +0100

    New commit

--- TP5_exo2.c
+++ TP5_exo2.c
@@ -3,11 +3,11 @@
 
 int main(){
     int nbValeurs;
-    printf("Combien de valeurs dans le tableau ? (maximum 100)");
+    printf("Combien de valeurs dans le tableau ? (maximum 100) :\n");
     scanf("%d", &nbValeurs);
     int tableau[nbValeurs];
     for (int i = 0; i < nbValeurs; ++i) {
-        printf("Saisir la valeur n°%d :", i+1);
+        printf("Saisir la valeur numero %d : ", i+1);
         scanf("%d", &tableau[i]);
     }
 
