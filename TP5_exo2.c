6e7605780f9fec2a0c90fd1a53cc65fbf841c32a
abret <arthur.bret@edu.ece.fr>
2022-11-17 09:44:54 +0100

    New commit

--- TP5_exo2.c
+++ TP5_exo2.c
@@ -2,13 +2,16 @@
 #include "librairieTP5.h"
 
 int main(){
-    int nbValeurs;
+    int nbValeurs, valeurLaPlusGrande;
     printf("Combien de valeurs dans le tableau ? (maximum 100) :\n");
     scanf("%d", &nbValeurs);
     int tableau[nbValeurs];
     for (int i = 0; i < nbValeurs; ++i) {
         printf("Saisir la valeur numero %d :", i+1);
         scanf("%d", &tableau[i]);
+    }
+    for (int i = 1; i < (nbValeurs-1); ++i) {
+        tableau[i+1] > tableau[i] ? valeurLaPlusGrande = tableau[i+1] : valeurLaPlusGrande = tableau[i];
     }
 
 
